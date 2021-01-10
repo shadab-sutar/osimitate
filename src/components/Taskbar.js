7eb7b6ab6e0813b3dd19757180d2a97db3985b55
SH <SH@Shadabs-MacBook-Air.local>
2021-01-10 14:19:18 +0530

    start click method implementation

--- src/components/Taskbar.js
+++ src/components/Taskbar.js
@@ -1,21 +1,30 @@
+import React, { Component } from 'react';
 import logo from '../resources/winStart.jpg';
-function Taskbar() {
-    return (
-        <div>
-            <div className="bar">
-                <div className="startLogo">
-                    <img
-                        src={logo}
-                        style={{
-                            width: '50px',
-                            height: '35px',
-                            margin: '2px'
-                        }}
-                    />
+class Taskbar extends Component {
+
+    onStart = () => {
+        alert("Start clicked");
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="bar">
+                    <div className="startLogo">
+                        <img
+                            src={logo}
+                            style={{
+                                width: '50px',
+                                height: '35px',
+                                margin: '2px'
+                            }}
+                            onClick={this.onStart}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Taskbar;
